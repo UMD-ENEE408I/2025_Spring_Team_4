a7b0e0354de2415aa3d3b6304b89df2f09b58344
ango4@terpmail.umd.edu <ango4@terpmail.umd.edu>
2025-02-04 13:05:08 -0500

    Finished 1-4 Part A

--- Lab2/Lab2_PartA.py
+++ Lab2/Lab2_PartA.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import scipy.signal as signal
 import soundfile as sf
 import wave
+import scipy.io.wavfile
+
 # Read the audio file
 wav_file = wave.open("./human_voice.wav", "rb") 
 framerate = wav_file.getframerate()
@@ -20,4 +22,11 @@ plt.title("Human voice")
 plt.plot(audio_data)
 plt.show()
 #Downsample!
-write_file = wave.open("./new_human_voice.wav", "wb")
+downsample_factor = framerate / 8000 #Target 8 kHz
+downsample_audio = audio_data[::int(downsample_factor)]
+out_wav = wave.open("./new_human_voice.wav", 'wb')
+outframerate = out_wav.setframerate(8000)
+outchannelnum = out_wav.setnchannels(num_channels)
+outsamplewidth = out_wav.setsampwidth(samplewidth)
+
+out_wav.writeframes(downsample_audio.tobytes())
