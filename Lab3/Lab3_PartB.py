b1d28b6ef76f370fa98fd74e9cd53cb1aa7e1ec0
ango4@terpmail.umd.edu <ango4@terpmail.umd.edu>
2025-02-11 13:20:02 -0500

    i hope to god this works

--- Lab3/Lab3_PartB.py
+++ Lab3/Lab3_PartB.py
@@ -7,8 +7,11 @@ model = YOLO("yolov8n.pt")
 # Display model information (optional)
 model.info()
 
-# Train the model on the COCO8 example dataset for 25 epochs
-results = model.train(data="coco8.yaml", epochs=25, imgsz=640)
+# Train the model on the COCO8 example dataset for 10 epochs
+results = model.train(data="coco8.yaml", epochs=10, imgsz=640)
+print("\n Now starting part 2 \n")
 
  #Now let's try use YOLO to make a model to detect the burgerbot!!!!
 
+botresults = model.train(data = r"C:\Users\andyn\408I\2025_Spring_Team_4-1\Lab3\data.yaml", epochs = 15, imgsz = 640, batch = 16)
+model.val()
