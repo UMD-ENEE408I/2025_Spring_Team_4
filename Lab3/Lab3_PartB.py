184e2b7fd90fc28a4dfc42b599cdf6b59da69dbf
ango4@terpmail.umd.edu <ango4@terpmail.umd.edu>
2025-02-11 12:56:56 -0500

    yaml file, requirements,and part B updated

---
+++ Lab3/Lab3_PartB.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+import torch
+
+# Load a COCO-pretrained YOLOv8n model
+model = YOLO("yolov8n.pt")
+
+# Display model information (optional)
+model.info()
+
+# Train the model on the COCO8 example dataset for 25 epochs
+results = model.train(data="coco8.yaml", epochs=25, imgsz=640)
+
+ 
